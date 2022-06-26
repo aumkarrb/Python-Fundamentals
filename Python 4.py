3b9ab2410129e5765b5afdd2897e021d1b9e0c63
aumkarrb <74492028+aumkarrb@users.noreply.github.com>
2022-06-26 22:23:41 +0530

    Update Python 4.py

--- Python 4.py
+++ Python 4.py
@@ -2,7 +2,7 @@ a = int(input(" Enter a no. "))
 b = int(input(" Enter another no."))
 product = 0
 count = a
-while count > 1.:
+while count > 1 :
      count = count - 1
      product = product + a
 print("The product b/w {} and {} is equal to {}".format(a,b,product))
